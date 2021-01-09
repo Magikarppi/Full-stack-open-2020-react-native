d02e10cfc9835d1bc858dc5cfe97b81e2d7956e4
Sami H <samih90@gmail.com>
2021-01-09 15:43:29 +0200

    Started working on graphql queries

--- rate-repository-app/src/components/hooks/useRepositories.js
+++ rate-repository-app/src/components/hooks/useRepositories.js
@@ -1,17 +1,28 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+
+import { GET_REPOSITORIES } from '../../graphql/queries';
 
 const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
 
   const fetchRepositories = async () => {
-    setLoading(true);
+    // setLoading(true);
+    try {
+      const response = await fetch(
+        'http://192.168.1.105:5000/api/repositories'
+      );
+      const jsonData = await response.json();
 
-    const response = await fetch('exp://192.168.1.105:5000/api/repositories');
-    const jsonData = await response.json();
-
-    setLoading(false);
-    setRepositories(jsonData);
+      // setLoading(false);
+      setRepositories(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
